690cbbada5b017206c0499b42c906901bf057fcf
Daniel Bailey <Dbailey0210@gmail.com>
2018-02-11 14:00:18 -0600

    continue working on Jquery folder assignment

---
+++ JQuery/app.js
@@ -0,0 +1,35 @@
+$( document ).ready(function() {
+
+
+    //Objective 1
+    $buttonDiv = $('<div></div>');
+    $buttonDiv.text('Objective 1');
+    $buttonDiv.addClass('button-div');
+    $('body').append($buttonDiv);
+
+    $button = $('<button></button>');
+    $button.addClass('main-button');
+    $button.text('Click Me!');
+    $buttonDiv.append($button);
+
+    $button.click(function() {
+        alert('This is a nice message!');
+    });
+
+
+    //Objective 2
+    $buttonTwo = $('<button></button>');
+    $buttonTwo.addClass('main-button');
+    $buttonTwo.text('Click Me!');
+    $buttonDiv.append($buttonTwo);
+
+    let message = 'This is text for the text box';
+    $('<p>' + message + '</p>').appendTo($buttonDiv);
+
+    $buttonTwo.click(function() {
+        alert(message);
+    });
+
+
+
+});
